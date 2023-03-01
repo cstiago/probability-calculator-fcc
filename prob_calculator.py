8f3d3241ca613a9cd923c56a633b95af6389e779
Tiago Cunha <121774112+cstiago@users.noreply.github.com>
2023-03-01 16:20:33 -0300

    feat(calculator): add contents list to constructor

--- prob_calculator.py
+++ prob_calculator.py
@@ -2,5 +2,12 @@ import copy
 import random
 
 class Hat:
+    def __init__(self, **colors):
+        self.contents = list()
+
+        for color in colors:
+            for ball in range(colors[color]):
+                self.contents.append(color)
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    return 0
