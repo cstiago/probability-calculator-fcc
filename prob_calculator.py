9d5d207f647e66221088fccba37608f884744c01
Tiago Cunha <121774112+cstiago@users.noreply.github.com>
2023-03-01 18:31:49 -0300

    feat(calculator): add experiment function logic

--- prob_calculator.py
+++ prob_calculator.py
@@ -23,4 +23,25 @@ class Hat:
         return balls_drawn
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    return 0
+    success_count = 0
+
+    for i in range(num_experiments):
+        hat_copy = copy.deepcopy(hat)
+        balls_drawn = hat_copy.draw(num_balls_drawn)
+        expected_balls_copy = copy.deepcopy(expected_balls)
+
+        success = True
+
+        for ball in expected_balls_copy.keys():
+            if balls_drawn.count(ball) < expected_balls_copy[ball]:
+                success = False
+                break
+            else:
+                balls_drawn.remove(ball)
+
+        if success:
+            success_count += 1
+
+    probability = success_count / num_experiments
+
+    return probability
