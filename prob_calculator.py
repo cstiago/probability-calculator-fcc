a443a9ae7bfa8f3af41f410f7c763d3677c6dcc3
Tiago Cunha <121774112+cstiago@users.noreply.github.com>
2023-03-01 17:04:53 -0300

    feat(calculator): add draw method

--- prob_calculator.py
+++ prob_calculator.py
@@ -9,5 +9,15 @@ class Hat:
             for ball in range(colors[color]):
                 self.contents.append(color)
 
+    def draw(self, number):
+        balls_to_draw = random.sample(self.contents, number)
+
+        if balls_to_draw <= self.contents:
+            for ball in copy.copy(self.contents):
+                if ball in balls_to_draw:
+                    self.contents.remove(ball)
+
+        return balls_to_draw
+
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     return 0
